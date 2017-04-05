ddf505b71b214920371188ad469631dd3021a150
Luke Hogan <hoganld@gmail.com>
2017-04-05 15:56:45 -0700

    Default values for Yellin config variables

--- lib/yellin.rb
+++ lib/yellin.rb
@@ -2,10 +2,18 @@ require "yellin/engine"
 require "yellin/acts_as_user"
 
 module Yellin
-  # VERSION = '0.1.0'
   mattr_accessor :user_class, :default_from_address, :app_name
 
   def self.user_class
     @@user_class.constantize
   end
+
+  def self.default_from_address
+    @@default_from_address || 'noreply@exmaple.com'
+  end
+
+  def self.app_name
+    @@app_name || Rails.application.class.parent
+  end
+
 end
