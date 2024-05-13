51a5b09e4e1c0863cb145cffaf2c24ded12e32de
Luke Hogan <hoganld@gmail.com>
2024-05-13 10:47:57 -0600

    Remove obsolete/unnecessary Minitest config in test_helper.rb

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,11 +6,6 @@ ActiveRecord::Migrator.migrations_paths = [File.expand_path("../test/dummy/db/mi
 ActiveRecord::Migrator.migrations_paths << File.expand_path('../db/migrate', __dir__)
 require "rails/test_help"
 
-# Filter out Minitest backtrace while allowing backtrace from other libraries
-# to be shown.
-Minitest.backtrace_filter = Minitest::BacktraceFilter.new
-
-
 # Load fixtures from the engine
 if ActiveSupport::TestCase.respond_to?(:fixture_paths=)
   ActiveSupport::TestCase.fixture_paths = [File.expand_path("fixtures", __dir__)]
